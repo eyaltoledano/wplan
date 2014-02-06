250264657d3bae3e2e5215b18aa13e27e7cf3227
Eyal Toledano <etoledano@playigl.com>
2014-02-05 19:24:39 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wplan::Application.config.secret_key_base = '2af292303a4d808ce70f56e31254aa760442005b5574162449048f5e4d06f817735a5dfbd9ae17c3ef33f9b70e8ca54724a95571469d64d11b1a8c43934a2adb'
